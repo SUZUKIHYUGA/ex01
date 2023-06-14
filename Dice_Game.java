e45f397e12cd97a9fe3a1f19e19fba3294e7468b
SUZUKIHYUGA <s1300077@u-aizu.ac.jp>
2023-06-14 10:37:08 +0900

    Won/lost message modified

--- Dice_Game.java
+++ Dice_Game.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 class Dice_Game{
 
     public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
         Random rand = new Random();
         int rand1 = rand.nextInt(6) + 1;
         int rand2 = rand.nextInt(6) + 1;
@@ -10,5 +14,7 @@ class Dice_Game{
         System.out.println("Die 1: " + rand1);
         System.out.println("Die 2: " + rand2);
         System.out.println("Total value: " + (rand1 + rand2));
+        if((rand1 + rand2) > 7) System.out.println(name + " won!");
+        else System.out.println(name + " lost");
     }
 }
